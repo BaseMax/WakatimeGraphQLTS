13e39abbcb6f806c493459fd7fe7797752996f05
pooya_dehghan <pooyadehghan@yahoo.com>
2023-08-23 14:27:06 +0330

    started out test writing in e2e-spec.ts

--- src/modules/team/dto/update-team.dto.ts
+++ src/modules/team/dto/update-team.dto.ts
@@ -1,7 +1,7 @@
 import { Field, InputType, ID } from '@nestjs/graphql';
 import { IsOptional, IsString, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
-import { User } from 'src/modules/user/user.model';
+import { User } from '../../../modules/user/user.model';
 
 @InputType()
 export class UpdateTeamDto {
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaClient } from '@prisma/client';
+import { PrismaService } from '../src/modules/prisma/prisma.service';
+import { UserService } from '../src/modules/user/user.service';
+const gql = '/graphql';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaClient;
+  let userService: UserService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
+    prisma = app.get(PrismaService);
+    userService = app.get(UserService);
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  beforeEach(async () => {
+    //running before each
+  });
+
+  describe('auth module', () => {
+    it('should successfuly sign up a new user with appropriate username and password', async () => {
+      const userRegisterInput = {
+        username: 'asghar',
+        password: '123321pp',
+        email: 'asghar@gmail.com',
+        bio: 'this is a bio from pooya life is much cooler than it seems',
+      };
+      const response = await request(app.getHttpServer())
+        .post(gql)
+        .send({
+          query: `
+          mutation {
+            signUp(input:{username: "${userRegisterInput.username}", password: "${userRegisterInput.password}", bio: "${userRegisterInput.bio}", email: "${userRegisterInput.email}"}) {
+                id
+                username    
+                userAtId
+                bio
+                token
+                avatar
+                notificationStatus
+            }
+          }
+        `,
+        });
+      expect(response.status).toBe(200);
+      expect(response.body.data.signUp.username).toBeDefined();
+      expect(response.body.data.signUp.id).toBeDefined();
+      expect(response.body.data.signUp.username).toBe(
+        userRegisterInput.username,
+      );
+    });
+  });
+
+  afterAll(async () => {
+    await prisma.user.deleteMany();
+    await app.close();
+    await prisma.$disconnect();
   });
 });
